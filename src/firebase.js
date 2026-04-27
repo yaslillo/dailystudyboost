ac3b19500a1176b30034b340b264fb2042687d32
Tu Nombre <yasna@gmail.com>
2026-04-27 10:28:41 -0400

    fix firebase config production

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdWkHadPAcc4ofxOEf2kmifiHjkPm_nhg",
+  apiKey: "AIzaSyDdwKHadpACC4oXoEF2kmifHjKpM_nhg",
   authDomain: "dailystudyboost-f5160.firebaseapp.com",
   projectId: "dailystudyboost-f5160",
   storageBucket: "dailystudyboost-f5160.appspot.com",
   messagingSenderId: "848138886017",
-  appId: "1:848138886017:web:abcc95f3cfca3b48262680"
+  appId: "1:848138886017:web:abc..."
 };
 
 const app = initializeApp(firebaseConfig);
