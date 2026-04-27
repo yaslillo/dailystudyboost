65347468a8d4c2a2d96aac99bc0fc988d572e47e
Tu Nombre <yasna@gmail.com>
2026-04-27 11:13:23 -0400

    fix firebase real credentials

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,13 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDdwKHadpACC4oXoEF2kmifHjKpM_nhg",
+  apiKey: "AIzaSyDdWkHadPAcC4ofxOEf2kmifiHjkPm_nhg",
   authDomain: "dailystudyboost-f5160.firebaseapp.com",
   projectId: "dailystudyboost-f5160",
   storageBucket: "dailystudyboost-f5160.firebasestorage.app",
@@ -11,7 +15,5 @@ const firebaseConfig = {
   appId: "1:848138886017:web:abcc95f3cfca3b48262680"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
